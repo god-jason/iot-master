1afe88a45aff9646360887cf21547468ab628dc7
jason <jason@zgwit.com>
2025-06-03 17:46:32 +0800

    提交更新

--- main.go
+++ main.go
@@ -4,21 +4,23 @@ import (
 	"embed"
 	"encoding/json"
 	_ "github.com/busy-cloud/boat-ui"
-	"github.com/busy-cloud/boat/app"
 	"github.com/busy-cloud/boat/apps"
 	"github.com/busy-cloud/boat/log"
 	_ "github.com/god-jason/iot-master/internal"
 	"github.com/god-jason/iot-master/protocol"
 )
 
+//go:embed assets
+var assets embed.FS
+
 //go:embed pages
 var pages embed.FS
 
-//go:embed protocols
-var protocols embed.FS
-
 //go:embed manifest.json
 var manifest []byte
+
+//go:embed protocols
+var protocols embed.FS
 
 func init() {
 	//注册协议
@@ -28,10 +30,11 @@ func init() {
 	apps.Pages().EmbedFS(pages, "pages")
 
 	//注册为内部插件
-	var a app.App
+	var a apps.App
 	err := json.Unmarshal(manifest, &a)
 	if err != nil {
 		log.Fatal(err)
 	}
+	a.Assets = assets
 	apps.Register(&a)
 }
