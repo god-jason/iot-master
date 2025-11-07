a77ba78bb552bacf8eb67ad1239d0084d61cba75
Jason <jason@zgwit.com>
2025-11-07 11:51:04 +0800

    设备参数修改功能

--- iot/device-api.go
+++ iot/device-api.go
@@ -1,16 +1,23 @@
 package iot
 
 import (
+	"time"
+
 	"github.com/busy-cloud/boat/api"
 	"github.com/busy-cloud/boat/curd"
 	"github.com/busy-cloud/boat/db"
 	"github.com/gin-gonic/gin"
+	"xorm.io/xorm/schemas"
 )
 
 func init() {
 	//物模型
 	api.Register("GET", "iot/device/:id/model", curd.ApiGet[DeviceModel]())
 	api.Register("POST", "iot/device/:id/model", deviceModelUpdate)
+
+	//参数
+	api.Register("GET", "iot/device/:id/setting/:name", deviceSetting)
+	api.Register("POST", "iot/device/:id/setting/:name", deviceSettingUpdate)
 
 }
 
@@ -48,4 +55,55 @@ func deviceModelUpdate(ctx *gin.Context) {
 	}
 
 	api.OK(ctx, &model)
+}
+
+type DeviceSetting struct {
+	Id      string         `json:"id" xorm:"pk"`
+	Name    string         `json:"name" xorm:"pk"`
+	Content map[string]any `json:"content,omitempty" xorm:"text"`
+	Created time.Time      `json:"created,omitempty" xorm:"created"`
+}
+
+func deviceSetting(ctx *gin.Context) {
+	id := ctx.Param("id")
+	name := ctx.Param("name")
+
+	var setting DeviceSetting
+	has, err := db.Engine().ID(schemas.PK{id, name}).Get(&setting)
+	if err != nil {
+		api.Error(ctx, err)
+		return
+	}
+	if !has {
+		api.OK(ctx, nil)
+		return
+	}
+
+	api.OK(ctx, &setting.Content)
+}
+
+func deviceSettingUpdate(ctx *gin.Context) {
+	id := ctx.Param("id")
+	name := ctx.Param("name")
+
+	var content map[string]any
+	err := ctx.ShouldBind(&content)
+	if err != nil {
+		api.Error(ctx, err)
+		return
+	}
+
+	var setting DeviceSetting
+	setting.Id = id
+	setting.Name = name
+	setting.Content = content
+
+	_, err = db.Engine().ID(schemas.PK{id, name}).Delete(new(DeviceSetting)) //不管有没有都删掉
+	_, err = db.Engine().ID(schemas.PK{id, name}).Insert(&setting)
+	if err != nil {
+		api.Error(ctx, err)
+		return
+	}
+
+	api.OK(ctx, content)
 }
