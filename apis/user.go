bc237561b8c2e6d881e37348f08c90b04a05d449
Jason <jason@zgwit.com>
2026-03-10 11:08:40 +0800

    bug

--- apis/user.go
+++ apis/user.go
@@ -4,13 +4,11 @@ import "time"
 
 // User 用户
 type User struct {
-	Id        string    `json:"id" xorm:"pk"`
-	Name      string    `json:"name,omitempty"`
-	Email     string    `json:"email,omitempty"`
-	Cellphone string    `json:"cellphone,omitempty"`
-	Admin     bool      `json:"admin,omitempty"`
-	Disabled  bool      `json:"disabled,omitempty"`
-	Created   time.Time `json:"created,omitempty" xorm:"created"`
+	Id       string    `json:"id" xorm:"pk"`
+	Name     string    `json:"name,omitempty"`
+	Admin    bool      `json:"admin,omitempty"`
+	Disabled bool      `json:"disabled,omitempty"`
+	Created  time.Time `json:"created,omitempty" xorm:"created"`
 }
 
 // Password 密码
