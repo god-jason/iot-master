d7f0182ed784b06d5cd73ca903dec4846a021bcb
Jason <jason@zgwit.com>
2026-04-10 09:48:27 +0800

    合并启动脚本

--- main.go
+++ main.go
@@ -1,39 +1,135 @@
-package iot_master
+package main
 
 import (
 	"embed"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	_ "github.com/god-jason/iot-master/apis"
 	"github.com/god-jason/iot-master/iot"
+	"github.com/god-jason/iot-master/pkg/boot"
+	_ "github.com/god-jason/iot-master/pkg/broker"
+	"github.com/god-jason/iot-master/pkg/log"
 	"github.com/god-jason/iot-master/pkg/menu"
+	_ "github.com/god-jason/iot-master/pkg/oem"
 	"github.com/god-jason/iot-master/pkg/page"
+	"github.com/god-jason/iot-master/pkg/service"
 	"github.com/god-jason/iot-master/pkg/store"
+	_ "github.com/god-jason/iot-master/pkg/table"
+	"github.com/god-jason/iot-master/pkg/version"
 	"github.com/god-jason/iot-master/pkg/web"
+	_ "github.com/god-jason/iot-master/weixin"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
-
-//go:embed menu.json
-var menuJson []byte
-
-//go:embed pages
-var pages embed.FS
-
-//go:embed protocols
-var protocols embed.FS
 
 //go:embed dist/browser
 var www embed.FS
 
 func init() {
+	//菜单
+	menu.File("menu.json")
 
-	page.PagesFS = store.PrefixFS(&pages, "pages")
-
-	err := menu.Content(menuJson)
-	if err != nil {
-		panic(err)
-	}
+	//页面
+	page.PagesFS = store.Dir("pages")
 
 	//加载协议
-	iot.Protocols = store.PrefixFS(&protocols, "protocols")
+	iot.Protocols = store.Dir("protocols")
 
 	//前端页面
 	web.StaticFS(www, "/", "dist/browser", "index.html")
+}
+
+func Startup() error {
+	viper.SetConfigName("iot-master")
+
+	err := boot.Startup()
+	if err != nil {
+		//_ = boot.Shutdown()
+		return err
+	}
+
+	//异步执行，避免堵塞
+	go func() {
+		//启动服务
+		err := web.Serve()
+		if err != nil {
+			//安全退出
+			//_ = boot.Shutdown()
+			log.Error(err)
+		}
+	}()
+
+	log.Info("main started")
+
+	return nil
+}
+
+func Shutdown() error {
+	log.Info("main shutdown")
+
+	return boot.Shutdown()
+}
+
+func main() {
+
+	help := pflag.BoolP("help", "h", false, "show help")
+	install := pflag.BoolP("install", "i", false, "install as service")
+	uninstall := pflag.BoolP("uninstall", "u", false, "uninstall service")
+	showVersion := pflag.BoolP("version", "v", false, "show version")
+
+	pflag.Parse()
+	if *help {
+		pflag.PrintDefaults()
+		return
+	}
+
+	err := service.Register(Startup, Shutdown)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *install {
+		log.Info("install service")
+		err = service.Install()
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else if *uninstall {
+		log.Info("uninstall service")
+		err = service.Uninstall()
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	} else if *showVersion {
+		version.Print()
+		return
+	}
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		s := <-sigs
+		log.Info("signal received ", s)
+
+		//_ = boot.Shutdown()
+		err := service.Stop()
+		if err != nil {
+			log.Error(err)
+		}
+
+		time.AfterFunc(10*time.Second, func() {
+			os.Exit(0)
+		})
+	}()
+
+	err = service.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	println("bye")
 }
