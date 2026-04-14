87c3e5ded285fd59b374748931903aba5a786993
Jason <jason@zgwit.com>
2026-04-14 23:59:11 +0800

    OEM

--- apis/user.go
+++ apis/user.go
@@ -5,9 +5,10 @@ import "time"
 // User 用户
 type User struct {
 	Id       string    `json:"id" xorm:"pk"`
+	TenantId string    `json:"tenant_id,omitempty"` //多租户
 	Name     string    `json:"name,omitempty"`
 	Admin    bool      `json:"admin,omitempty"`
-	TenantId string    `json:"tenant_id,omitempty"` //多租户
+	Oem      string    `json:"oem,omitempty"`
 	Disabled bool      `json:"disabled,omitempty"`
 	Created  time.Time `json:"created,omitempty" xorm:"created"`
 }
